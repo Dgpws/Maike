f92dc54b2ddda51f2e717dde7770607d1e7df9d6
Dgpws <142136919+Dgpws@users.noreply.github.com>
2023-11-20 10:42:03 +0800

    Create Weixin AD.js

---
+++ Weixin AD.js
@@ -0,0 +1,2 @@
+# 微信去广告
+Wechat.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js,type=http-response,pattern=^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad
